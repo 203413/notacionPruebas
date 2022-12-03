0c0d40e66ca177edac4e4207d89708898c32de89
Alan Alberto Gómez Gómez <77992634+203429@users.noreply.github.com>
2022-12-03 14:38:03 -0600

    style bug fixed

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,6 +4,8 @@ import NavBar from '../components/global/NavBar';
 import Index from '../components/modules/conjunto_potencia/Index';
 import Operaciones from '../components/modules/operaciones/operaciones';
 import Notacion from '../components/modules/notacion/Notacion';
+import Notacion_quiz from '../components/modules/notacion/Notacion_Quiz';
+import Notacion_validar from '../components/modules/notacion/Notacion_Validar';
 
 const AppRouter = () => {
     
@@ -12,13 +14,15 @@ const AppRouter = () => {
             <div className='row mb-5 pb-3'>
                 <NavBar />
             </div>
-            <div className='container-xxl m-3 pt-1'>
+            <div className='container-xxl pt-1'>
                 <Router>
                     <Routes>
                         {/* Agregar sus rutas aquí */}
                         <Route path='/potencia' element={<Index />} />
                         <Route path='/operaciones' element={<Operaciones />} />
                         <Route path='/notacion' element={<Notacion />} />
+                        <Route path='/notacion/quiz' element={<Notacion_quiz />} />
+                        <Route path='/notacion/validar' element={<Notacion_validar />} />
                     </Routes>
                 </Router>
             </div>
